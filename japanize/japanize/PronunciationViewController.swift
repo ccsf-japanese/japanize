2fdbe5fe1b16f497f53b3ebd0a508ea9495c996e
Dylan James SMith <dsmith95@mail.ccsf.edu>
2016-03-17 03:26:54 -0700

    Set up preliminary layout and removing build errors. Will upload resized images shortly.

---
+++ japanize/japanize/PronunciationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PronunciationViewController.swift
+//  japanize
+//
+//  Created by Dylan Smith on 3/17/16.
+//  Copyright © 2016 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class PronunciationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
