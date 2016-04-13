ba2412e9e9bf93c3a42e0a8112ebbb20085eb25e
Tom Horvath <thorvath@mail.ccsf.edu>
2016-04-12 20:47:51 -0700

    Fix formatting in Kanji class

--- japanize/Kanji.swift
+++ japanize/Kanji.swift
@@ -6,28 +6,29 @@
 //  Copyright © 2016 Codepath. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 class Kanji {
+  let strokes: [Stroke]
     
-    let strokes: [Stroke]
+  init() {
+    let path = NSBundle.mainBundle().pathForResource("054a8", ofType: "svg")!
+    let content: NSString = try! String(contentsOfFile: path)
+    let regex = try! NSRegularExpression(pattern: " d=\"(.*)\"", options: [])
     
-    init() {
-      var pathStrings: [String] = []
-      let path = NSBundle.mainBundle().pathForResource("054a8", ofType: "svg")!
-      let content: NSString = try! String(contentsOfFile: path)
-      let regex = try! NSRegularExpression(pattern: " d=\"(.*)\"", options: [])
-      
-      let matches = regex.matchesInString(content as String, options: [], range: NSRange(location: 0, length: content.length))
-      for match in matches {
-        let range = match.rangeAtIndex(1)
-        pathStrings.append(content.substringWithRange(range))
-      }
-      
-      var strokes: [Stroke] = []
-        for pathString in pathStrings {
-            strokes.append(Stroke(pathString: pathString))
-      }
-      self.strokes = strokes
+    var pathStrings: [String] = []
+    let matches = regex.matchesInString(content as String,
+                                        options: [],
+                                        range: NSRange(location: 0, length: content.length))
+    for match in matches {
+      let range = match.rangeAtIndex(1)
+      pathStrings.append(content.substringWithRange(range))
     }
+      
+    var strokes: [Stroke] = []
+    for pathString in pathStrings {
+      strokes.append(Stroke(pathString: pathString))
+    }
+    self.strokes = strokes
+  }
 }
