a2c2f4a641e2367c9536d3c64ce9259ad8023e0c
Tom Horvath <thorvath@mail.ccsf.edu>
2016-04-16 16:35:59 -0700

    Fix bug with Chapter data being incorrect

--- japanize/ProgressViewController.swift
+++ japanize/ProgressViewController.swift
@@ -67,7 +67,7 @@ class ProgressViewController: UIViewController, UITableViewDataSource, UITableVi
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ChapterCell") as! ChapterCell
-        cell.chapter = book!.chapters[indexPath.row];
+        cell.chapter = book!.chapters[indexPath.section];
         cell.selectionStyle = .None
         return cell
     }
