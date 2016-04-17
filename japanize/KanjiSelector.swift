23c05ccba16f599f820b8101d235a1895bd6fd71
Xinxin Xie <pro_cindy@hotmail.com>
2016-04-16 19:15:10 -0700

    Add KanjiSelector.swift

---
+++ japanize/KanjiSelector.swift
@@ -0,0 +1,25 @@
+//
+//  KanjiSelector.swift
+//  japanize
+//
+//  Created by Xinxin Xie on 4/16/16.
+//  Copyright © 2016 Codepath. All rights reserved.
+//
+
+import Foundation
+
+class KanjiSelector {
+    init() {
+//        let fileManager = NSFileManager.defaultManager()
+//        let path = NSBundle.mainBundle().resourcePath!
+//        let items = try fileManager.contentsOfDirectoryAtPath(path){
+//            
+//        } catch {
+//            
+//        }
+        
+    }
+    
+    
+}
+
