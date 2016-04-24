50d69fb0eb06962d1854ccbbc306cde23a99e53b
Tom Horvath <thorvath@mail.ccsf.edu>
2016-04-23 20:48:03 -0700

    Save book and only download it once during the lifetime of the app

--- japanize/JapanizeClient.swift
+++ japanize/JapanizeClient.swift
@@ -5,6 +5,8 @@ let serverBaseURL = NSURL(string: "https://horvathtom.com/jp/api/")
 
 class JapanizeClient : AFHTTPSessionManager {
   
+  var cachedBook: Book?
+  
   class var sharedInstance: JapanizeClient {
     struct Static {
       static let instance = JapanizeClient(baseURL: serverBaseURL)
@@ -13,18 +15,23 @@ class JapanizeClient : AFHTTPSessionManager {
   }
   
   func book(completion: (book: Book?, error: NSError?) -> ()) {
-    GET(
-      "book",
-      parameters: nil,
-      progress: nil,
-      success: { (operation: NSURLSessionDataTask!, response: AnyObject?) -> Void in
-        let book = Book(dictionary: response as! NSDictionary)
-        completion(book: book, error: nil)
-      },
-      failure: { (operation: NSURLSessionDataTask?, error: NSError!) -> Void in
-        print("error getting book")
-        completion(book: nil, error: error)
-    })
+    // Use cached value if available
+    if let book = cachedBook {
+      completion(book: book, error: nil)
+    } else {
+      GET(
+        "book",
+        parameters: nil,
+        progress: nil,
+        success: { (operation: NSURLSessionDataTask!, response: AnyObject?) -> Void in
+          self.cachedBook = Book(dictionary: response as! NSDictionary)
+          completion(book: self.cachedBook, error: nil)
+        },
+        failure: { (operation: NSURLSessionDataTask?, error: NSError!) -> Void in
+          print("error getting book")
+          completion(book: nil, error: error)
+      })
+    }
   }
   
 }
